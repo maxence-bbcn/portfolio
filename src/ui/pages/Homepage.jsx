7c1f60132d8a9153a98ddefd2c11d7e12b9054df
maxence <maxence.barbancon@outlook.fr>
2025-11-04 16:05:22 +0100

    first half 100% responsive

--- src/ui/pages/Homepage.jsx
+++ src/ui/pages/Homepage.jsx
@@ -151,9 +151,9 @@ export default function Homepage() {
             obligent à trouver des solutions détournées et rendent chaque tâche
             plus complexe qu&apos;elle ne devrait l&apos;être ?
           </p>
-          <div className="border-primary-gradient overflow-x-visible rounded-2xl p-px">
-            <div className="bg-primary-bg flex w-full flex-col rounded-2xl md:relative md:flex-row">
-              <p className="px-8 pt-6 text-xl leading-loose md:w-3/5 md:pb-6 md:text-base lg:w-2/3 lg:p-16 lg:text-lg 2xl:text-2xl">
+          <div className="border-primary-gradient relative overflow-x-visible rounded-2xl p-px">
+            <div className="bg-primary-bg flex w-full flex-col rounded-2xl md:relative md:flex-row max-md:landscape:flex-row">
+              <p className="px-8 pt-6 text-xl leading-loose md:w-3/5 md:pb-6 md:text-base lg:w-2/3 lg:p-16 lg:text-lg 2xl:text-2xl max-md:landscape:w-3/5 max-md:landscape:pb-6 max-md:landscape:text-base">
                 Vos équipes
                 <span className="font-semibold"> perdent en efficacité</span>,
                 l&apos;information circule mal entre les services, et
@@ -170,12 +170,13 @@ export default function Homepage() {
                 contraintes qu&apos;à
                 <span className="font-semibold"> créer de la valeur</span>.
               </p>
-              <div className="mask-notifications relative h-80 overflow-x-visible overflow-y-clip sm:min-h-[500px] md:h-auto md:min-h-0 md:w-1/2 lg:w-1/3 xl:overflow-y-visible">
+              <span className="relative h-70 overflow-x-hidden overflow-y-clip md:h-0 xl:h-0 max-md:landscape:h-0"></span>
+              <div className="mask-notifications">
                 <img
                   src={IPhoneAsset}
                   alt="Illustration d'un téléphone saturé par des notifications"
                   width="970px"
-                  className="absolute -right-10 xl:top-1/5 xl:left-0 2xl:top-0"
+                  className="max-xs:w-full absolute w-full max-w-none max-md:landscape:w-full"
                 />
               </div>
             </div>
@@ -233,81 +234,81 @@ export default function Homepage() {
             des tâches, centraliser vos données ou structurer vos workflows,
             chaque solution est pensée autour de vos besoins réels.
           </p>
-          <div className="flex grid-cols-12 grid-rows-12 flex-col gap-10 md:grid md:items-stretch md:gap-3">
+          <div className="flex grid-cols-12 grid-rows-12 flex-col gap-10 md:grid md:items-stretch md:gap-3 max-md:landscape:grid max-md:landscape:items-stretch max-md:landscape:gap-3">
             {/* Carte accompagnement */}
-            <div className="border-primary-800 relative flex w-full flex-col overflow-x-visible rounded-2xl border pt-6 md:col-span-4 md:col-start-1 md:row-span-7 md:row-start-1">
+            <div className="border-primary-800 relative flex w-full flex-col overflow-x-visible rounded-2xl border pt-6 md:col-span-4 md:col-start-1 md:row-span-7 md:row-start-1 max-md:landscape:col-span-4 max-md:landscape:col-start-1 max-md:landscape:row-span-7 max-md:landscape:row-start-1">
               <div className="flex flex-col gap-8 px-6">
-                <h3 className="text-2xl font-medium md:text-lg xl:text-2xl 2xl:text-3xl">
+                <h3 className="text-2xl font-medium md:text-lg xl:text-2xl 2xl:text-3xl max-md:landscape:text-lg">
                   Un accompagnement sur-mesure,{" "}
                   <strong>du début à la fin</strong>
                 </h3>
-                <p className="text-lg leading-normal font-extralight italic md:text-sm lg:text-base xl:text-lg xl:leading-relaxed">
+                <p className="text-lg leading-normal font-extralight italic md:text-sm lg:text-base xl:text-lg xl:leading-relaxed max-md:landscape:text-sm">
                   Écoute, pédagogie et réactivité : vous avancez en confiance,
                   avec un interlocuteur unique qui comprend vos enjeux et reste
                   disponible.
                 </p>
               </div>
-              <span className="relative h-70 overflow-x-hidden overflow-y-clip md:h-0 xl:h-0"></span>
+              <span className="relative h-70 overflow-x-hidden overflow-y-clip md:h-0 xl:h-0 max-md:landscape:h-0"></span>
               <div className="mask-sms">
                 <img
                   src={TextsAsset}
                   alt="Illustration d'un échange de sms"
                   width="598px"
-                  className="absolute top-0 left-0 w-full max-w-none md:top-10 md:left-7/12 md:w-5/9 lg:w-1/2 xl:top-12 xl:left-0 xl:w-[130%] 2xl:w-[110%]"
+                  className="absolute top-0 left-0 w-full max-w-none md:top-10 md:left-7/12 md:w-5/9 lg:w-1/2 xl:top-12 xl:left-0 xl:w-[130%] 2xl:w-[110%] max-md:landscape:top-10 max-md:landscape:-left-15 max-md:landscape:w-[170%]"
                 />
               </div>
             </div>
             {/* Carte interface */}
-            <div className="border-primary-800 relative flex w-full flex-col gap-2 overflow-x-visible rounded-2xl border pt-6 md:col-start-5 md:col-end-13 md:row-span-3 md:row-start-1 md:flex-row">
-              <div className="flex flex-col gap-8 px-6 md:w-2/3 md:py-2 lg:w-3/5 lg:gap-6">
-                <h3 className="text-2xl font-medium md:text-lg xl:text-2xl 2xl:text-3xl">
+            <div className="border-primary-800 relative flex w-full flex-col gap-2 overflow-x-visible rounded-2xl border pt-6 md:col-start-5 md:col-end-13 md:row-span-3 md:row-start-1 md:flex-row max-md:landscape:col-start-5 max-md:landscape:col-end-13 max-md:landscape:row-span-3 max-md:landscape:row-start-1 max-md:landscape:flex-row">
+              <div className="flex flex-col gap-8 px-6 md:w-2/3 md:py-2 lg:w-3/5 lg:gap-6 max-md:landscape:w-2/3 max-md:landscape:py-2">
+                <h3 className="text-2xl font-medium md:text-lg xl:text-2xl 2xl:text-3xl max-md:landscape:text-lg">
                   Une interface pensée pour <strong>vos utilisateurs</strong>
                 </h3>
-                <p className="text-lg leading-normal font-extralight italic md:text-sm lg:text-base xl:text-lg xl:leading-relaxed">
+                <p className="text-lg leading-normal font-extralight italic md:text-sm lg:text-base xl:text-lg xl:leading-relaxed max-md:landscape:text-sm">
                   L&apos;interface est simple à prendre en main, agréable à
                   utiliser au quotidien, et parfaitement adaptée à vos usages
                   métier.
                 </p>
                 <Button>Planifiez un appel</Button>
               </div>
-              <div className="relative h-64 overflow-visible md:h-full md:w-1/3 lg:w-2/5">
+              <div className="relative h-64 overflow-visible md:h-full md:w-1/3 lg:w-2/5 max-md:landscape:h-full max-md:landscape:w-1/3">
                 <img
                   src={UiModsAsset}
                   alt="Illusatration d'une interface utilisateur"
-                  className="1-2xl:w-[110%] 1-2xl:-top-5 absolute left-1/10 w-5/6 max-w-none md:-left-2/10 md:w-[150%] lg:-top-10 lg:w-[130%] xl:-top-8 xl:left-0 xl:w-[120%] 2xl:w-11/12"
+                  className="1-2xl:w-[110%] 1-2xl:-top-5 absolute left-1/10 w-5/6 max-w-none md:-left-2/10 md:w-[150%] lg:-top-10 lg:w-[130%] xl:-top-8 xl:left-0 xl:w-[120%] 2xl:w-11/12 max-md:landscape:-left-2/10 max-md:landscape:w-[150%]"
                 />
               </div>
             </div>
             {/* Carte mise en prod */}
-            <div className="border-primary-800 col-end-9 flex w-full flex-col items-stretch gap-2 overflow-hidden rounded-2xl border py-6 md:col-start-5 md:row-span-4 md:row-start-4">
-              <div className="relative flex flex-col gap-8 px-6 md:gap-4 lg:gap-8">
-                <h3 className="text-2xl font-medium md:text-lg xl:text-2xl 2xl:text-3xl">
+            <div className="border-primary-800 flex w-full flex-col items-stretch gap-2 overflow-hidden rounded-2xl border py-6 md:col-start-5 md:col-end-9 md:row-span-4 md:row-start-4 max-md:landscape:col-start-5 max-md:landscape:col-end-9 max-md:landscape:row-span-4 max-md:landscape:row-start-4">
+              <div className="relative flex flex-col gap-8 px-6 md:gap-4 lg:gap-8 max-md:landscape:gap-4">
+                <h3 className="text-2xl font-medium md:text-lg xl:text-2xl 2xl:text-3xl max-md:landscape:text-lg">
                   Mise en ligne <strong>rapide</strong>, sans compromis sur la{" "}
                   <strong>qualité</strong>
                 </h3>
                 <div className="-gap-2 flex h-fit flex-row">
-                  <p className="w-3/4 text-lg leading-normal font-extralight italic md:text-sm lg:text-base xl:text-lg xl:leading-relaxed">
+                  <p className="w-3/4 text-lg leading-normal font-extralight italic md:text-sm lg:text-base xl:text-lg xl:leading-relaxed max-md:landscape:text-sm">
                     Vos outils sont opérationnels rapidement pour que vous
                     puissiez gagner en efficacité sans attendre.
                   </p>
-                  <div className="relative h-60 w-1/4 md:h-fit lg:w-1/4">
+                  <div className="relative h-60 w-1/4 md:h-fit lg:w-1/4 max-md:landscape:h-fit">
                     <img
                       src={ZapAsset}
                       alt="Illustration d'un éclair"
-                      className="1-2xl:-top-20 1-2xl:-left-44 1-2xl:w-[450%] absolute -top-6 -right-20 w-80 max-w-none md:top-0 md:-left-24 md:w-60 lg:-top-10 lg:-left-30 lg:w-76 xl:-top-10 xl:-left-30 xl:w-72 2xl:-top-32 2xl:-left-48 2xl:w-[400%]"
+                      className="1-2xl:-top-20 1-2xl:-left-44 1-2xl:w-[450%] absolute -top-6 -right-20 w-80 max-w-none md:top-0 md:-left-24 md:w-60 lg:-top-10 lg:-left-30 lg:w-76 xl:-top-10 xl:-left-30 xl:w-72 2xl:-top-32 2xl:-left-48 2xl:w-[400%] max-md:landscape:top-0 max-md:landscape:w-60"
                     />
                   </div>
                 </div>
               </div>
             </div>
             {/* Carte évolutivité */}
-            <div className="border-primary-800 flex w-full flex-col gap-2 overflow-hidden rounded-2xl border pt-6 md:col-start-9 md:col-end-13 md:row-span-4 md:row-start-4">
+            <div className="border-primary-800 flex w-full flex-col gap-2 overflow-hidden rounded-2xl border pt-6 md:col-start-9 md:col-end-13 md:row-span-4 md:row-start-4 max-md:landscape:col-start-9 max-md:landscape:col-end-13 max-md:landscape:row-span-4 max-md:landscape:row-start-4">
               <div className="relative flex h-full flex-col gap-8">
-                <h3 className="pr-6 pl-6 text-2xl font-medium md:text-lg xl:text-2xl 2xl:text-3xl">
+                <h3 className="pr-6 pl-6 text-2xl font-medium md:text-lg xl:text-2xl 2xl:text-3xl max-md:landscape:text-lg">
                   Une solution pensée pour <strong>grandir avec vous</strong>
                 </h3>
                 <div className="flex h-full flex-col">
-                  <p className="w-5/6 pr-6 pl-6 text-lg leading-normal font-extralight italic md:w-full md:text-sm lg:text-base xl:w-3/4 xl:text-lg xl:leading-relaxed">
+                  <p className="w-5/6 pr-6 pl-6 text-lg leading-normal font-extralight italic md:w-full md:text-sm lg:text-base xl:w-3/4 xl:text-lg xl:leading-relaxed max-md:landscape:w-full max-md:landscape:text-sm">
                     Architecture modulaire, fonctionnalités extensibles, votre
                     outil s&apos;adapte à vos futurs besoins.
                   </p>
@@ -315,22 +316,22 @@ export default function Homepage() {
                     <img
                       src={GraphAsset}
                       alt="Illustration d'une courbe qui monte"
-                      className="ml-auto w-3/4 max-w-none md:ml-4 md:w-full lg:ml-8 xl:absolute xl:-top-6 xl:right-0 xl:w-3/4 2xl:-top-12"
+                      className="ml-auto w-3/4 max-w-none md:ml-4 md:w-full lg:ml-8 xl:absolute xl:-top-6 xl:right-0 xl:w-3/4 2xl:-top-12 max-md:landscape:mt-8 max-md:landscape:ml-4 max-md:landscape:w-full"
                     />
                   </div>
                 </div>
               </div>
             </div>
             {/* Carte compatibilité */}
-            <div className="border-primary-800 relative flex w-full flex-col gap-8 overflow-hidden rounded-2xl border px-6 pt-6 md:col-span-6 md:col-start-7 md:row-start-8 md:row-end-12 md:items-start lg:row-end-11">
-              <h3 className="text-2xl font-medium md:text-xl lg:w-1/2 xl:text-2xl 2xl:text-3xl">
+            <div className="border-primary-800 relative flex w-full flex-col gap-8 overflow-hidden rounded-2xl border px-6 pt-6 md:col-span-6 md:col-start-7 md:row-start-8 md:row-end-12 md:items-start lg:row-end-11 max-md:landscape:col-span-6 max-md:landscape:col-start-7 max-md:landscape:row-start-8 max-md:landscape:row-end-11">
+              <h3 className="text-2xl font-medium md:text-lg lg:w-1/2 xl:text-2xl 2xl:text-3xl max-md:landscape:text-lg">
                 <strong>Compatible</strong> avec vos outils existants
               </h3>
-              <p className="text-lg leading-normal font-extralight italic md:text-base lg:w-1/2 xl:text-lg xl:leading-relaxed">
+              <p className="text-lg leading-normal font-extralight italic md:text-sm lg:w-1/2 xl:text-lg xl:leading-relaxed max-md:landscape:text-sm">
                 Connexions possibles avec vos logiciels, API ou plateformes déjà
                 en place.
               </p>
-              <span className="relative h-30 overflow-x-hidden overflow-y-clip md:h-0"></span>
+              <span className="relative h-30 overflow-x-hidden overflow-y-clip md:h-0 max-md:landscape:h-0"></span>
               <div className="mask-apps">
                 <img
                   src={AppsAsset}
@@ -340,20 +341,20 @@ export default function Homepage() {
               </div>
             </div>
             {/* Carte solution sur mesure */}
-            <div className="border-primary-800 relative flex w-full flex-col gap-8 overflow-hidden rounded-2xl border px-6 pt-6 md:col-span-6 md:col-start-1 md:row-start-8 md:row-end-12 lg:row-end-11">
-              <h3 className="text-2xl font-medium md:text-xl xl:text-2xl 2xl:text-3xl">
+            <div className="border-primary-800 relative flex w-full flex-col gap-8 overflow-hidden rounded-2xl border px-6 pt-6 md:col-span-6 md:col-start-1 md:row-start-8 md:row-end-12 lg:row-end-11 max-md:landscape:col-span-6 max-md:landscape:col-start-1 max-md:landscape:row-start-8 max-md:landscape:row-end-11">
+              <h3 className="text-2xl font-medium md:text-lg xl:text-2xl 2xl:text-3xl max-md:landscape:text-lg">
                 Une solution vraiment faire <strong>pour vous</strong>
               </h3>
-              <p className="text-lg leading-normal font-extralight italic md:text-base lg:w-3/4 xl:w-2/3 xl:text-lg xl:leading-relaxed 2xl:w-1/2">
+              <p className="text-lg leading-normal font-extralight italic md:text-sm lg:w-3/4 xl:w-2/3 xl:text-lg xl:leading-relaxed 2xl:w-1/2 max-md:landscape:text-sm">
                 Chaque projet part de vos contraintes réelles : vous obtenez une
                 solution unique, conçue pour fonctionner dans votre contexte.
               </p>
-              <span className="relative h-30 overflow-x-hidden overflow-y-clip md:h-0"></span>
+              <span className="relative h-30 overflow-x-hidden overflow-y-clip md:h-0 max-md:landscape:h-0"></span>
               <div className="mask-fingerprint">
                 <img
                   src={FingerprintAsset}
                   alt="Illustration d'une emprunte digitale"
-                  className="w-full max-w-none lg:w-4/5 xl:w-2/3 2xl:w-3/5"
+                  className="w-full max-w-none lg:w-4/5 xl:w-2/3 2xl:w-3/5 max-md:landscape:w-4/5"
                 />
               </div>
             </div>
