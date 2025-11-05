71a940f2c4fc54e10c1a498e546e8d6e081d9d30
maxence <maxence.barbancon@outlook.fr>
2025-11-05 14:51:36 +0100

    contact and footer 100% responsive

--- src/ui/components/CustomFooter.jsx
+++ src/ui/components/CustomFooter.jsx
@@ -4,20 +4,20 @@ import CustomInput from "./CustomInput";
 export default function CustomFooter() {
   return (
     <div className="flex flex-col gap-16">
-      <div className="flex flex-col gap-16 bg-pattern-diag self-center w-full justify-center items-center py-16 xl:py-32 px-4 xl:px-32">
-        <h2 className="text-center text-3xl font-title xl:text-5xl px-4 xl:px-32">
+      <div className="bg-pattern-diag flex w-full flex-col items-center justify-center gap-16 self-center px-4 py-16 xl:px-32 xl:py-32">
+        <h2 className="font-title px-4 text-center text-3xl xl:px-32 xl:text-5xl">
           Des projets web plus classiques ? Je suis aussi là pour ça.
         </h2>
-        <p className="text-primary-200 text-center leading-relaxed px-4 xl:px-32 xl:text-2xl">
+        <p className="text-primary-200 px-4 text-center leading-relaxed xl:px-32 xl:text-2xl">
           Mes compétences en design et développement web me permettent de
           réaliser des projets variés, du site vitrine au e-commerce. Ensemble,
           nous pouvons donner vie à votre projet !
         </p>
-        <div className="bg-linear-to-b from-primary-400 to-primary-800/80 p-px rounded-2xl xl:w-3/5 ">
+        <div className="from-primary-400 to-primary-800/80 w-full rounded-2xl bg-linear-to-b p-px md:w-11/12 2xl:w-3/5">
           <form
             action="fonction à créer"
             method="post"
-            className="flex flex-col items-end gap-5 p-8 bg-linear-180 from-primary-800 to-primary-bg rounded-2xl"
+            className="from-primary-800 to-primary-bg flex flex-col items-end gap-5 rounded-2xl bg-linear-180 p-8"
           >
             <CustomInput fieldName={"Nom"} placeholder={"Votre nom"} />
             <CustomInput fieldName={"Prénom"} placeholder={"Votre prénom"} />
@@ -25,7 +25,7 @@ export default function CustomFooter() {
               fieldName={"Mail"}
               placeholder={"Votre adresse e-mail"}
             />
-            <div className="flex flex-col gap-1 w-full">
+            <div className="flex w-full flex-col gap-1">
               <label htmlFor={"Message"}>Message *</label>
               <textarea
                 rows={7}
@@ -34,48 +34,48 @@ export default function CustomFooter() {
                 id={"Message"}
                 name={"Message"}
                 placeholder={"Votre message"}
-                className="border rounded-lg p-6 bg-primary-bg border-primary-500/50"
+                className="bg-primary-bg border-primary-500/50 rounded-lg border p-6"
               />
             </div>
             <Button type="submit">J&apos;envoie un message</Button>
           </form>
         </div>
       </div>
-      <footer className="px-4 xl:px-32">
-        <h2 className="text-center text-2xl font-bold py-16 xl:text-4xl xl:text-left">
+      <footer className="px-4 lg:px-16 xl:px-32">
+        <h2 className="py-16 text-center text-2xl font-bold lg:text-left xl:text-4xl">
           Travaillons ensemble !
         </h2>
-        <div className="border-t-[1px] border-b-[1px] border-white/20 flex flex-col xl:flex-row xl:justify-between gap-16 py-16">
-          <div className="flex flex-col gap-10 xl:gap-14 items-center xl:items-start xl:w-2/5">
-            <p className="font-semibold text-2xl">Maxence Barbançon</p>
-            <p className="xl:text-xl leading-loose text-primary-200 text-center xl:text-left">
+        <div className="flex flex-col gap-16 border-t-[1px] border-b-[1px] border-white/20 py-16 lg:flex-row lg:justify-between">
+          <div className="flex flex-col items-center gap-10 lg:w-2/5 lg:items-start lg:gap-14">
+            <p className="text-2xl font-semibold">Maxence Barbançon</p>
+            <p className="text-primary-200 text-center leading-loose lg:text-left xl:text-xl">
               Je conçois des applications web sur mesure pensées autour de vos
               besoins réels.
             </p>
-            <p className="font-semibold text-2xl">Contact :</p>
+            <p className="text-2xl font-semibold">Contact :</p>
             <div className="flex flex-row gap-2">
               <img src="src/ui/assets/icons/mail.svg" alt="icône e-mail" />
               <p>maxence.barbancon@gmail.com</p>
             </div>
             <Button>Je réserve un appel gratuit</Button>
           </div>
-          <div className="flex flex-col items-center xl:items-start gap-6">
-            <p className="font-semibold text-2xl">Services</p>
+          <div className="flex flex-col items-center gap-6 lg:items-start">
+            <p className="text-2xl font-semibold">Services</p>
             <p className="text-primary-200">Développement sur mesure</p>
             <p className="text-primary-200">Site vitrine et portfolio</p>
             <p className="text-primary-200">Autres projets web</p>
           </div>
-          <div className="flex flex-col items-center xl:items-start gap-6">
-            <p className="font-semibold text-2xl">À propos</p>
+          <div className="flex flex-col items-center gap-6 lg:items-start">
+            <p className="text-2xl font-semibold">À propos</p>
             <p className="text-primary-200">À propos de moi</p>
           </div>
-          <div className="flex flex-col items-center xl:items-start gap-6">
-            <p className="font-semibold text-2xl">Pages légales</p>
+          <div className="flex flex-col items-center gap-6 lg:items-start">
+            <p className="text-2xl font-semibold">Pages légales</p>
             <p className="text-primary-200">Mentions légales</p>
           </div>
         </div>
-        <div className="flex flex-col xl:flex-row-reverse xl:justify-between items-center gap-10 py-8">
-          <div className="flex flex-row gap-4 items-center">
+        <div className="flex flex-col items-center gap-10 py-8 lg:flex-row-reverse lg:justify-between">
+          <div className="flex flex-row items-center gap-4">
             <img src="src/ui/assets/logos/linkedin.svg" alt="logo LinkedIn" />
             <p>Connectons-nous sur LinkedIn !</p>
           </div>
