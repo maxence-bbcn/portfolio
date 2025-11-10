d32003ca72fa11bfc7fa3db0980ede40fa8c2203
maxence <maxence.barbancon@outlook.fr>
2025-11-10 10:23:43 +0100

    Revert button - animation to do later

--- src/ui/components/Button.jsx
+++ src/ui/components/Button.jsx
@@ -2,9 +2,10 @@ export default function Button({ children, type = "button", className }) {
   return (
     <button
       type={type}
-      className={`${className} btn-shine h-fit w-fit rounded-full text-base text-white md:text-sm lg:text-base xl:text-xl`}
+      className={`${className} button-bg-gradient h-fit w-fit rounded-full px-6 py-2 text-base text-white md:text-sm lg:text-base xl:text-xl`}
     >
-      <div className="button-bg-gradient">{children}</div>
+      {/* <div className="button-bg-gradient"></div> */}
+      {children}
     </button>
   );
 }
