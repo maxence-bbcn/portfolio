8f332ecbed327665fa4e0ef1f5eada721366b2a1
maxence <maxence.barbancon@outlook.fr>
2025-11-25 11:52:38 +0100

    page scroll fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {} from "react-router";
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+  ScrollRestoration,
+} from "react-router-dom";
 import "./index.css";
 import AboutPage from "./ui/pages/AboutPage.jsx";
 import Homepage from "./ui/pages/Homepage.jsx";
@@ -9,25 +15,38 @@ import "./ui/styles/effects.css";
 import "./ui/styles/gradients.css";
 import "./ui/styles/masks.css";
 
+const RootLayout = () => {
+  return (
+    <>
+      <ScrollRestoration />
+      <Outlet />
+    </>
+  );
+};
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Homepage />,
-  },
-  {
-    path: "/projects",
-    element: <ProjectsPage />,
-  },
-  {
-    path: "/about",
-    element: <AboutPage />,
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <Homepage />,
+      },
+      {
+        path: "/projects",
+        element: <ProjectsPage />,
+      },
+      {
+        path: "/about",
+        element: <AboutPage />,
+      },
+    ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}>
-      <Homepage />
-    </RouterProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
